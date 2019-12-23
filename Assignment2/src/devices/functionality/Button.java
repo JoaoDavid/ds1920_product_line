63d99d89b190ee32ee9b28f808e512f927852764
unknown <joaodavid512@gmail.com>
2019-12-23 23:14:53 +0000

    finished button class

--- Assignment2/src/devices/functionality/Button.java
+++ Assignment2/src/devices/functionality/Button.java
@@ -1,5 +1,26 @@
 package devices.functionality;
 
+import com.bezirk.middleware.Bezirk;
+import com.bezirk.middleware.java.proxy.BezirkMiddleware;
+
+import events.ButtonPressEvent;
+
+
 public class Button {
+	
+	private Bezirk bezirk;
+
+	public Button() {
+		BezirkMiddleware.initialize();
+        bezirk = BezirkMiddleware.registerZirk("Wearable's Button");
+	}
+	
+	public void pressButton() {
+        final ButtonPressEvent buttonPressEvent = new ButtonPressEvent();
+
+        //sends the event
+        bezirk.sendEvent(buttonPressEvent);
+        System.err.println("Published button press event: " + buttonPressEvent.toString());
+    }
 
 }
--- Assignment2/src/events/ButtonPressEvent.java
+++ Assignment2/src/events/ButtonPressEvent.java
@@ -1,7 +1,28 @@
 package events;
 
+import java.time.LocalDateTime;
+
 import com.bezirk.middleware.messages.Event;
 
 public class ButtonPressEvent extends Event {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private final LocalDateTime moment;
+
+	public ButtonPressEvent() {
+		moment = LocalDateTime.now();
+	}
+
+	public LocalDateTime getMoment() {
+		return moment;
+	}
+	
+	public String toString() {
+		return moment.toString();
+	}
 
 }
