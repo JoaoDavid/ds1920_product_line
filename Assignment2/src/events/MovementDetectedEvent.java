ad6c77d96cb6bb6caa5b8965e4cea82965e5f1e0
unknown <joaodavid512@gmail.com>
2019-12-23 23:33:40 +0000

    finished movement detected event

--- Assignment2/src/events/MovementDetectedEvent.java
+++ Assignment2/src/events/MovementDetectedEvent.java
@@ -1,5 +1,33 @@
 package events;
 
+import java.time.LocalTime;
+
+import com.bezirk.middleware.messages.Event;
+
 public class MovementDetectedEvent extends Event {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String location;
+	private LocalTime time;
+	
+	public MovementDetectedEvent(String location) {
+		this.location = location;
+		time = LocalTime.now();
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public LocalTime getTime() {
+		return time;
+	}
+	
+	public String toString() {
+		return location + " - " + time;
+	}
 }
