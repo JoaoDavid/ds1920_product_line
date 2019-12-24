feab0fdab508985db9182d94ffee46b5c8980ba2
unknown <joaodavid512@gmail.com>
2019-12-24 01:15:33 +0000

    finished activity tracker

--- Assignment2/src/devices/functionality/ActivityTracker.java
+++ Assignment2/src/devices/functionality/ActivityTracker.java
@@ -1,5 +1,29 @@
 package devices.functionality;
 
-public class ActivityTracker {
+import java.util.Timer;
+import java.util.TimerTask;
 
+import com.bezirk.middleware.Bezirk;
+import com.bezirk.middleware.java.proxy.BezirkMiddleware;
+
+import events.ActivityEvent;
+
+public class ActivityTracker {
+	
+	private Bezirk bezirk;
+	
+	public ActivityTracker() {
+		BezirkMiddleware.initialize();
+        bezirk = BezirkMiddleware.registerZirk("Activity Tracker");		
+	}
+	
+	public void startTrackingActivity() {
+		new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+            	ActivityEvent activityEvent = new ActivityEvent(); 
+                bezirk.sendEvent(activityEvent);
+            }
+        }, 1000, 1000);
+	}
 }
--- Assignment2/src/events/ActivityEvent.java
+++ Assignment2/src/events/ActivityEvent.java
@@ -1,5 +1,28 @@
 package events;
 
+import java.time.LocalDateTime;
+
+import com.bezirk.middleware.messages.Event;
+
 public class ActivityEvent extends Event {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private LocalDateTime time;
+	
+	public ActivityEvent() {
+		time = LocalDateTime.now();
+	}
+
+	public LocalDateTime getTime() {
+		return time;
+	}
+	
+	public String toString() {
+		return time.toString();
+	}
 
 }
