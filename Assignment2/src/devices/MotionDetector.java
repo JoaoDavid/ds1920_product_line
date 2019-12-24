1786ff73634b81edf1fde3a1f608fb070aa3bb88
unknown <joaodavid512@gmail.com>
2019-12-24 00:00:05 +0000

    finished Motion Detector device

--- Assignment2/src/devices/MotionDetector.java
+++ Assignment2/src/devices/MotionDetector.java
@@ -1,5 +1,33 @@
 package devices;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import com.bezirk.middleware.Bezirk;
+import com.bezirk.middleware.java.proxy.BezirkMiddleware;
+
+import events.MovementDetectedEvent;
+
 public class MotionDetector {
+	
+	private Bezirk bezirk;
+	private String location;
+	
+	public MotionDetector(String location) {
+		this.location = location;
+		BezirkMiddleware.initialize();
+        bezirk = BezirkMiddleware.registerZirk("Motion Detector");		
+	}
+	
+	public void startMonitoringMovement() {
+		new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+            	MovementDetectedEvent movementDetectedEvent = 
+                		new MovementDetectedEvent(location); 
+                bezirk.sendEvent(movementDetectedEvent);
+            }
+        }, 1000, 1000);
+	}
 
 }
