334535001ce46347beef18b62233612e2c1005b6
Ye Yang <fc49521@alunos.fc.ul.pt>
2019-12-29 23:15:18 +0000

    Added XML parser.

---
+++ Assignment2/src/fileReader/Aspects.java
@@ -0,0 +1,10 @@
+package fileReader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Aspects {
+	public static final List<String> aspects = Arrays.asList("ActivityTrackerAspect.aj","ActivityUpdateEventAspect.aj","AlertSelectorAspect.aj",
+			"ButtonAspect.aj","ButtonPressEventAspect.aj","ChangeContactAspect.aj","InactivityAlertAspect.aj","MovementAlertAspect.aj","MovementDetectedEventAspect.aj",
+			"NotificationLEDAspect.aj","ReadExtraInputAspect.aj","pt_PT.aj","en_US.aj","LightSignalEventAspect.aj","SynthetizedVoiceAspect.aj");
+}
---
+++ Assignment2/src/fileReader/FileCreator.java
@@ -0,0 +1,95 @@
+package fileReader;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class FileCreator {
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException{
+		List<String> as = new ArrayList<>(Aspects.aspects);
+		
+		System.out.println("Inserir nome do ficheiro a ler: ");
+		Scanner in = new Scanner(System.in);
+		String xmlFileName = in.nextLine(); 
+		
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(new File(xmlFileName+".xml"));
+        
+        NodeList featureList = document.getElementsByTagName("feature");
+        for(int i = 0; i < featureList.getLength(); i++){
+        	Node f = featureList.item(i);
+    		Element feature = (Element) f;
+        	if(feature.getAttribute("automatic").equals("selected")){
+        		removeName(feature,as);
+        	}
+        	
+        	if(feature.getAttribute("manual").equals("selected")){
+        		removeName(feature,as);
+        	}
+        }
+        File file = new File("Hola.txt");
+        FileWriter writer = new FileWriter(file);
+        writer.write("src.includes = src/\n");
+        writer.write("src.excludes = ");
+        for (String string : as) {
+			writer.write("src/rules/"+string+ ",\\\n");
+		}
+        writer.close();
+        in.close();
+
+	}
+
+	private static void removeName(Element feature, List<String> aspects) {
+		String fName = feature.getAttribute("name");
+		if(fName.equals("Português")){
+			aspects.remove("pt_PT.aj");
+		}else if(fName.equals("Inglês")){
+			aspects.remove("en_US.aj");
+		}else if(fName.equals("VozSintetizada")){
+			aspects.remove("SynthetizedVoiceAspect.aj");
+		}else if(fName.equals("SinaisDeLuzes")){
+			aspects.remove("NotificationLEDAspect.aj");
+		}else if(fName.equals("Movimento")){
+			aspects.remove("MovementDetectedEventAspect.aj");
+			aspects.remove("MovementAlertAspect.aj");
+			aspects.remove("ReadExtraInputAspect.aj");
+			aspects.remove("ChangeContactAspect.aj");
+			aspects.remove("AlertSelectorAspect.aj");
+		}else if(fName.equals("Inatividade")){
+			aspects.remove("InactivityAlertAspect.aj");
+			aspects.remove("ActivityUpdateEventAspect.aj");
+			aspects.remove("ReadExtraInputAspect.aj");
+			aspects.remove("ChangeContactAspect.aj");
+			aspects.remove("AlertSelectorAspect.aj");
+		}else if(fName.equals("BotaoPressionado")){
+			aspects.remove("ButtonPressEventAspect.aj");
+			aspects.remove("ChangeContactAspect.aj");
+		}else if(fName.equals("MonitorDeAtividade")){
+			aspects.remove("ActivityTrackerAspect.aj");
+		}else if(fName.equals("Botao")){
+			aspects.remove("ButtonAspect.aj");
+		}else if(fName.equals("IndicadorLuminoso")){
+			aspects.remove("NotificationLEDAspect.aj");
+			aspects.remove("LightSignalEventAspect.aj");
+		}else if(fName.equals("LampadasInteligentes")){
+			aspects.remove("LightSignalEventAspect.aj");
+		}else if(fName.equals("DetetorMovimento")){
+			aspects.remove("MovementDetectedEventAspect.aj");
+		}
+	}
+}
